41f406130389c5179c6d231c450ed58c7d0df93c
ponizi <ponalexis@gmail.com>
2019-08-29 10:52:52 +0200

    First commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,61 @@
+class TasksController < ApplicationController
+before_action :authenticate_user!
+  def new
+    @categories = Category.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @category = Category.find(category_params)
+    @task.category = @category
+    if @task.save
+      flash[:notice] = "Bonjour"
+    else
+      flash[:notice] = redirect_to home_path
+    end
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @categories = Category.all
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+  end
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+  end
+  end
+
+
+  private
+
+  def task_params
+    params.permit(:title, :deadline, :description, :status)
+  end
+
+  def category_params
+    params.require(:Category)
+  end
+
+end
---
+++ db/migrate/20190828122715_add_status_task.rb
@@ -0,0 +1,4 @@
+class AddStatusTask < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
